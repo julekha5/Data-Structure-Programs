3dfd1222f158e5438a1d18668f9cbc375fcc3cc4
Julekha5 <mulani.julekha93@gmail.com>
2021-12-09 21:24:39 +0530

    Added:Insert Between Code

--- src/main/java/datastructure/linkedlist/LinkedList.java
+++ src/main/java/datastructure/linkedlist/LinkedList.java
@@ -2,23 +2,40 @@ package datastructure.linkedlist;
 
 public class LinkedList {
 	Node head;
-    Node tail;
+	Node tail;
 
-    /*
-     *  append the data in linked list
-     *
-     */
-    public void append(int data) {
-        Node nextNode = new Node(data);
-        if (head == null) {
-            head = nextNode;
-            tail = nextNode;
-        } else {
-            tail.next = nextNode;
-            tail = tail.next;
-        }
-    }
+	/*
+	 * create linked list
+	 *
+	 */
+	public void insert(int data) {
+		Node node = new Node(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			tail.next = node;
+			tail = tail.next;
+		}
+	}
 
+	/*
+	 * Method to insert between two nodes
+	 *
+	 */
+	public void insertBetweenNode(int a, int b, int c) {
+		Node newNode = new Node(c);
+		Node temp = head;
+		while (temp.next != null) {
+			if ((temp.data == a && temp.next.data == b) || (temp.data == a && temp.next.data == b)) {
+				Node afterc = temp.next;
+				temp.next = newNode;
+				temp.next.next = afterc;
+				break;
+			}
+			temp = temp.next;
+		}
+	}
 
 	/*
 	 * show method to display the linked list data
@@ -38,9 +55,10 @@ public class LinkedList {
 	// main method
 	public static void main(String[] args) {
 		LinkedList obj = new LinkedList();
-		obj.append(56);
-		obj.append(30);
-		obj.append(70);
+		obj.insert(56);
+		obj.insert(70);
+		obj.display();
+		obj.insertBetweenNode(56, 70, 30);
 		obj.display();
 	}
 }
