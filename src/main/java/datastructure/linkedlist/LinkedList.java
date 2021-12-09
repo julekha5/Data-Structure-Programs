d5cca1e7f843a9d8c40aa003bf8db81a64f76c16
Julekha5 <mulani.julekha93@gmail.com>
2021-12-09 20:11:05 +0530

    Added:Created Linked List By adding

--- src/main/java/datastructure/linkedlist/LinkedList.java
+++ src/main/java/datastructure/linkedlist/LinkedList.java
@@ -1,11 +1,45 @@
 package datastructure.linkedlist;
 
 public class LinkedList {
+	Node head;
+
+	/*
+	 * method to add linked list, push as first node will be the last node
+	 * 
+	 * @param data integer to be added in linked list
+	 */
+	public void addNodeMethod(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node temp = head;
+			head = newNode;
+			newNode.next = temp;
+		}
+	}
+
+	/*
+	 * show method to display the linked list data
+	 */
+	public void display() {
+		Node temp = head;
+		if (head == null) {
+			System.out.println("Linked list is empty");
+		} else {
+			while (temp != null) {
+				System.out.println(temp.data + "->");
+				temp = temp.next;
+			}
+		}
+	}
+
+	// main method
 	public static void main(String[] args) {
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-    }
+		LinkedList obj = new LinkedList();
+		obj.addNodeMethod(70);
+		obj.addNodeMethod(30);
+		obj.addNodeMethod(56);
+		obj.display();
+	}
 }
