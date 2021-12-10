76d42342ab7af2e76fbe8efad38313d7c8fabbaf
julekha5 <93564416+julekha5@users.noreply.github.com>
2021-12-10 12:30:53 +0530

    Added: Delete 40 from Linked List and show size 3

--- src/main/java/datastructure/linkedlist/LinkedList.java
+++ src/main/java/datastructure/linkedlist/LinkedList.java
@@ -9,7 +9,7 @@ public class LinkedList {
 	 * create linked list
 	 *
 	 */
-	public void insert(int data) {
+	public void add(int data) {
 		Node node = new Node(data);
 		if (head == null) {
 			head = node;
@@ -21,30 +21,67 @@ public class LinkedList {
 	}
 
 	/*
-	 *  delete the node in linked list
+	 * delete the node in linked list
 	 */
-    public void deleteNode(int key) {
-        if (head == null) {
-            System.out.println("Linked List is empty");
-        } else {
-            Node temp = head;
-            if (head.data == key) {
-                head = head.next;
-                size--;
-            }
-            while (temp != null && temp.next != null) {
-                if (temp.next.data == key) {
-                    temp.next = temp.next.next;
-                    size--;
-                }
-                temp = temp.next;
-            }
-        }
-    }
-   //check length of node
-    public void size() {
-        System.out.println("Size of the linked list is : " + size);
-    }
+	public void deleteNode(int key) {
+		if (head == null) {
+			System.out.println("Linked List is empty");
+		} else {
+			Node temp = head;
+			if (head.data == key) {
+				head = head.next;
+				size--;
+			}
+			while (temp != null && temp.next != null) {
+				if (temp.next.data == key) {
+					temp.next = temp.next.next;
+					size--;
+				}
+				temp = temp.next;
+			}
+		}
+	}
+
+	// check length of node
+	public void size() {
+		int size = 0;
+		Node current = head;
+		if (head == null) {
+			System.out.println("Size is 0");
+		}
+		current = head;
+		size = 1;
+		while (current.next != null) {
+			current = current.next;
+			size++;
+		}
+		System.out.println("List size linked list is:" + size);
+	}
+
+	// searchNode() will search for a given node in the list
+	public void searchNode(int data) {
+		Node current = head;
+		int i = 1;
+		boolean flag = false;
+		// Checks whether list is empty
+		if (head == null) {
+			System.out.println("List is empty");
+		} else {
+			while (current != null) {
+				// Compares node to be found with each node present in the list
+				if (current.data == data) {
+					flag = true;
+					break;
+				}
+				i++;
+				current = current.next;
+			}
+		}
+		if (flag)
+			System.out.println("Element is present in the list at the position : " + i);
+		else
+			System.out.println("Element is not present in the list");
+	}
 
 	/*
 	 * show method to display the linked list data
@@ -64,12 +101,13 @@ public class LinkedList {
 	// main method
 	public static void main(String[] args) {
 		LinkedList obj = new LinkedList();
-		obj.insert(56);
-		obj.insert(30);
-		obj.insert(40);
-		obj.insert(70);
+		obj.add(56);
+		obj.add(30);
+		obj.add(40);
+		obj.add(70);
 		obj.display();
 		obj.size();
+		obj.searchNode(40);
 		obj.deleteNode(40);
 		obj.display();
 		obj.size();
